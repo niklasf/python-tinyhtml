3ca21cb39152dc301c0db1e3bfd75e464ee70462
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-03-02 18:08:19 +0100

    test with python 3.10

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Markup :: HTML",
